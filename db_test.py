98b3e2c2bd0fb145f43cc2f9aa06cf7466a05b87
brianaparchment <34357764+brianaparchment@users.noreply.github.com>
2022-07-15 12:07:43 -0400

    add class structure

--- db_test.py
+++ db_test.py
@@ -1,28 +1,31 @@
-from sqlite3 import connect
-import mariadb
 import sys
+import time
+import unittest
+from sqlite3 import connect
 
+import mariadb
 from selenium import webdriver
 
-input_pass = input("Enter db password:")
+class Access_db(unittest.TestCase):
+    input_pass = input("Enter db password:")
 
-#connect to mariaDB
-try:
-    connect_db = mariadb.connect(
-        user = "user1",
-        password= input_pass,
-        host="localhost",
-        port=3306,
-        database="studentRecords" #name of db that will be accessed
+    #connect to mariaDB
+    try:
+        connect_db = mariadb.connect(
+            user = "user1",
+            password= input_pass,
+            host="localhost",
+            port=3306,
+            database="studentRecords" #name of db that will be accessed
+        )
+    except mariadb.Error as e:
+        print(f"Could not connect to MariaDB: {e}")
+        sys.exit(1) #exit due to error 
+
+    point = connect_db.cursor() #get cursor
+
+    point.execute(
+        "SELECT transcriptID FROM transcript"
     )
-except mariadb.Error as e:
-    print(f"Could not connect to MariaDB: {e}")
-    sys.exit(1) #exit due to error 
-
-point = connect_db.cursor() #get cursor
-
-point.execute(
-    "SELECT transcriptID FROM transcript"
-)
-for (transcriptID) in point:
-    print(f"TranscriptID: {transcriptID}")
+    for (transcriptID) in point:
+        print(f"TranscriptID: {transcriptID}")
