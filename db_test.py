c4ec2f533e736cb504d90f6aed4843cdf3c11c9c
brianaparchment <34357764+brianaparchment@users.noreply.github.com>
2022-07-15 11:47:43 -0400

    add user input for password

--- db_test.py
+++ db_test.py
@@ -1,17 +1,16 @@
-#this program allows users to search the contents of the database
-#the test will check the databse to see if the user input matches database records
-
 from sqlite3 import connect
 import mariadb
 import sys
 
 from selenium import webdriver
 
+input_pass = input("Enter db password:")
+
 #connect to mariaDB
 try:
     connect_db = mariadb.connect(
         user = "user1",
-        password="Zoe456bp",
+        password= input_pass,
         host="localhost",
         port=3306,
         database="studentRecords" #name of db that will be accessed
