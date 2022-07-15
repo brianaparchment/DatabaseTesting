abb16e62c1da46993ef47f5727614cec1928f56a
brianaparchment <34357764+brianaparchment@users.noreply.github.com>
2022-07-15 12:59:45 -0400

    add instructions for program and user input prompt

--- db_test.py
+++ db_test.py
@@ -24,6 +24,16 @@ except mariadb.Error as e:
 point = connect_db.cursor() #get cursor
 
 class Access_db(unittest.TestCase):
+    def setUp(self):
+        #instructions and description of each table in the database
+        self.instructions = ("This program allows users to access records from the database StudentRecords.\n")
+        self.descriptions = ("Table Names and Descriptions \n Transcript: contains each student's transcript information\n Student: contains each students ID and email\n Personal Info: contains demographic information about each student\n")
+        
+    def test_input(self):
+        print(self.instructions)
+        print(self.descriptions)
+        #user input to select specific records from database
+        self.user_input = input("Enter a table name to view all records from a specific table\n or Enter a studentID to view all records for a student: ")
   
         point.execute(
             "SELECT transcriptID FROM transcript"
