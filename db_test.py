5af915182f43c2636c9bc4b67ce8e2b03df423ca
brianaparchment <34357764+brianaparchment@users.noreply.github.com>
2022-07-15 09:23:02 -0400

    add imports

--- db_test.py
+++ db_test.py
@@ -1,2 +1,7 @@
 #this program allows users to search the contents of the database
 #the test will check the databse to see if the user input matches database records
+
+import mariadb
+import sys
+
+from selenium import webdriver
