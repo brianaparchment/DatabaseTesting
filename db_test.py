7fea7ed327d7a3fd6439831f04c230b8d3420636
brianaparchment <34357764+brianaparchment@users.noreply.github.com>
2022-07-15 09:29:01 -0400

    connect to db

--- db_test.py
+++ db_test.py
@@ -1,7 +1,18 @@
 #this program allows users to search the contents of the database
 #the test will check the databse to see if the user input matches database records
 
+from sqlite3 import connect
 import mariadb
 import sys
 
 from selenium import webdriver
+
+#connect to mariaDB
+try:
+    connect_db = mariadb.connect(
+        user = "user1",
+        password="Zoe456bp",
+        host="localhost",
+        port=3306,
+        database="studentRecords" #name of db that will be accessed
+    )
